63b417887173d4796da14f114f0158fb8cf2fa24
Georg Wiese <georgwiese@gmail.com>
2016-04-09 10:48:01 +0200

    Add analysis script

---
+++ py_2048_rl/analysis.py
@@ -0,0 +1,50 @@
+"""Script to analyze a given model."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from py_2048_rl.game import play
+from py_2048_rl.learning import learning
+from py_2048_rl.learning.model import FeedModel
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+TRAIN_DIR = "./train"
+
+
+def get_all_q_values(train_dir):
+  """Play randomly, compute q-values for all states."""
+
+  session = tf.Session()
+  model = FeedModel()
+  saver = tf.train.Saver()
+  saver.restore(session, tf.train.latest_checkpoint(train_dir))
+
+  get_q_values = learning.make_get_q_values(session, model)
+  experiences = learning.collect_experience(play.random_strategy, 100)
+  learning.print_memory_stats(experiences)
+
+  all_q_values = []
+  for experience in experiences:
+    all_q_values += list(get_q_values(experience.next_state))
+
+  return all_q_values
+
+
+def analyze():
+
+  plt.hist(get_all_q_values(TRAIN_DIR))
+  plt.show()
+
+
+def main(_):
+  """Main function."""
+
+  analyze()
+
+
+if __name__ == '__main__':
+  tf.app.run()
