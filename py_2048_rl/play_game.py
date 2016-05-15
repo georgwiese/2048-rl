b869c16ddc7afd1567a7ec83becd21ece6901a23
Georg Wiese <georgwiese@gmail.com>
2016-05-15 16:59:41 +0200

    Make play_game script more user friendly

--- py_2048_rl/play_game.py
+++ py_2048_rl/play_game.py
@@ -8,6 +8,8 @@ from py_2048_rl.game import play
 from py_2048_rl.learning import learning
 from py_2048_rl.learning.model import FeedModel
 
+import sys
+
 import tensorflow as tf
 import numpy as np
 
@@ -38,19 +40,31 @@ def make_greedy_strategy(train_dir, verbose=False):
   return greedy_strategy
 
 
-def play_game():
+def play_single_game():
   """Play a single game using the latest model snapshot in TRAIN_DIR."""
 
-  # s, _ = play.play(make_greedy_strategy(TRAIN_DIR, True),
-  #                  allow_unavailable_action=False)
-  # print(s)
+  s, _ = play.play(make_greedy_strategy(TRAIN_DIR, True),
+                   allow_unavailable_action=False)
+  print(s)
+
+
+def print_average_score():
+  """Prints the average score of 100 games."""
+
   print("Average Score: ", average_score(make_greedy_strategy(TRAIN_DIR)))
 
 
-def main(_):
+def main(args):
   """Main function."""
 
-  play_game()
+  if len(args) < 2:
+    print("Usage: %s (single|avg)" % args[0])
+    sys.exit(1)
+
+  if args[1] == "single":
+    play_single_game()
+  elif args[1] == "avg":
+    print_average_score()
 
 
 if __name__ == '__main__':
