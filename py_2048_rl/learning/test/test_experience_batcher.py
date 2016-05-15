b0b0b1685e405c7c1c3528c114984ffe6cc62101
Georg Wiese <georgwiese@gmail.com>
2016-05-15 16:42:18 +0200

    Fix test

---
+++ py_2048_rl/learning/test/test_experience_batcher.py
@@ -0,0 +1,39 @@
+"""Tests for Learning Algorithms."""
+
+from py_2048_rl.game.play import Experience
+from py_2048_rl.learning import experience_batcher
+
+from mock import Mock
+
+import numpy as np
+
+# pylint: disable=missing-docstring
+
+def test_experiences_to_batches():
+  experience_batcher.GAMMA = 0.5
+
+  state1 = np.arange(16).reshape((4, 4)) + 1
+  state2 = np.arange(16).reshape((4, 4)) + 2
+  state3 = np.arange(16).reshape((4, 4)) + 3
+  experiences = [Experience(state1, 1, 2, state2, False, False, [3]),
+                 Experience(state2, 3, 4, state3, True, False, [])]
+
+  run_inference = Mock(side_effect=[np.array([[0, 0, 0, -0.5],
+                                              [0, 0, 0, 0]])])
+
+  batcher = experience_batcher.ExperienceBatcher(None, run_inference, None,
+                                                 1.0 / 15.0)
+
+  state_batch, targets, actions = batcher.experiences_to_batches(experiences)
+
+  next_state_batch = np.array([state2.flatten(), state3.flatten()]) / 15.0
+  assert (run_inference.call_args_list[0][0][0] == next_state_batch).all()
+
+  expected_state_batch = np.array([state1.flatten(), state2.flatten()]) / 15.0
+  assert (state_batch == expected_state_batch).all()
+
+  # Targets should be -0.25 (0 reward + 0.5 * (-0.5) max Q) and -1
+  # (-1 reward, final state).
+  assert (targets == np.array([-0.25, -1])).all()
+
+  assert (actions == np.array([1, 3])).all()
